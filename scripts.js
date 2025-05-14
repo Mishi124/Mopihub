f870ad7898ce8610d88838613c397870d8b10f51
Mishi124 <mishi20242024@gmail.com>
2025-05-14 15:15:55 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,6 @@
 function enterSite() {
   document.getElementById("agePopup").style.display = "none";
   document.getElementById("mainContainer").style.display = "flex";
-
-  const grid = document.getElementById("videoGrid");
-  const imageURL = "https://i.ibb.co/SXy9FcgL/download.jpg";
-
-  // Inject 7 fake video blocks
-  for (let i = 0; i < 7; i++) {
-    const link = document.createElement("a");
-    link.href = "#";
-    link.className = "video-link";
-
-    const loader = document.createElement("div");
-    loader.className = "loader";
-
-    link.appendChild(loader);
-    grid.appendChild(link);
-
-    // Show loader immediately
-    setTimeout(() => {
-      loader.style.display = "block";
-    }, 0);
-  }
 }
 
 function denyAccess() {
